4db08a988d2a7c7adfe2544eaa2efad1c0882ebe
psalv <psalvato@uwo.ca>
2018-04-02 23:52:49 -0400

    finished linear regression

--- question1_linear_regression.py
+++ question1_linear_regression.py
@@ -1,11 +1,17 @@
 
 import numpy as np
+from matplotlib import pyplot as plt
+from math import floor, ceil
 
 
 def cost_function(X, y, theta):
 
     # Calculating the cost of the function (sum of squared errors)
-    return np.sum(((np.matmul(X, theta) - y) ** 2)) / (2 * np.size(X, 0))
+    return sum_of_squared_errors(X, y, theta) / (2 * np.size(X, 0))
+
+
+def sum_of_squared_errors(X, y, theta):
+    return np.sum(((np.matmul(X, theta) - y) ** 2))
 
 
 def gradient_descent(X, y, theta, alpha, threshold, costs):
@@ -40,8 +46,27 @@ def train_linear_regression():
     theta = np.array(([0], [0])).reshape(2, 1)
 
     costs = []
-    theta = gradient_descent(X, y, theta, 0.01, 10E-12, costs)
-    print(theta)
+    theta = gradient_descent(X, y, theta, 0.001, 10E-12, costs)
+
+    # # Plotting the learning curve
+    # plt.plot(costs)
+    # plt.ylabel("Cost")
+    # plt.xlabel("Iteration")
+    # plt.show()
+
+    # Calculating the linear regression line of prediction
+    x_range = np.array([floor(np.amin(data[1:, 0])), ceil(np.amax(data[1:, 0]))])
+    x_pred = np.array([x_range[0] * theta[1] + theta[0], x_range[1] * theta[1] + theta[0]])
+
+    # Plotting the data
+    plt.scatter(data[1:, 0], data[1:, 1])
+    plt.plot(x_range, x_pred)
+
+    print("Sum of squared errors: ", sum_of_squared_errors(X, y, theta))
+    print("Average sum of squared errors: ", costs[-1])
+
+    plt.show()
+
 
 
 train_linear_regression()
