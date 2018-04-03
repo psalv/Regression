b189a095f6d4762b6bf33e16862ab79f0c67befa
psalv <psalvato@uwo.ca>
2018-04-03 01:38:37 -0400

    Gradient descent and learning curve implemented

--- question2_logistic_regression.py
+++ question2_logistic_regression.py
@@ -1,16 +1,15 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-from math import log, exp
 
 
 def cost_function(X, y, theta):
     hx = sigmoid(theta, X)
-    return np.sum(np.multiply(-1 * y, log(hx)) - np.multiply(1 - y, log(1 - hx))) / len(y)
+    return np.sum(np.multiply(-1 * y, np.log10(hx)) - np.multiply(1 - y, np.log10(1 - hx))) / len(y)
 
 
 def sigmoid(theta, X):
-    return (1 + exp(-1 * np.transpose(theta) * X)) ** -1
+    return (1 + np.exp(-1 * np.matmul(X, theta))) ** -1
 
 
 def gradient_descent(X, y, theta, alpha, threshold, costs):
@@ -22,11 +21,8 @@ def gradient_descent(X, y, theta, alpha, threshold, costs):
     while len(costs) < 2 or abs(costs[-2] - costs[-1]) > threshold:
 
         # Calculating the changes to theta
-        theta0 = alpha * (m ** -1) * np.sum(np.matmul(X, theta) - y)
-        theta1 = alpha * (m ** -1) * np.sum(np.multiply(np.matmul(X, theta) - y, X[:, 1].reshape(len(X), 1)))
-
-        # Updating theta
-        theta = np.array(([theta[0][0] - theta0], [theta[1][0]] - theta1))
+        theta_ = (alpha * (m ** -1) * np.sum(np.multiply(sigmoid(theta, X) - y, X), 0)).reshape(np.size(theta), 1)
+        theta = theta - theta_
 
         # Saving the new cost
         costs.append(cost_function(X, y, theta))
@@ -51,28 +47,16 @@ def train_logistic_regression():
             y[i] = 1
 
     # Initializing thetas
-    theta = np.zeros(np.shape(X)[1])
+    theta = np.zeros([np.shape(X)[1], 1])
 
     costs = []
-    theta = gradient_descent(X, y, theta, 0.001, 10E-12, costs)
+    theta = gradient_descent(X, y, theta, 0.1, 10E-8, costs)
+    print(theta)
 
-    # # # Plotting the learning curve
+    # # Plotting the learning curve
     # plt.plot(costs)
     # plt.ylabel("J(theta)")
     # plt.xlabel("Iteration")
-    # plt.show()
-    #
-    # # Calculating the linear regression line of prediction
-    # # x_range = np.array([floor(np.amin(data[1:, 0])), ceil(np.amax(data[1:, 0]))])
-    # # x_pred = np.array([x_range[0] * theta[1] + theta[0], x_range[1] * theta[1] + theta[0]])
-    #
-    # # Plotting the data
-    # # plt.scatter(data[1:, 0], data[1:, 1])
-    # # plt.plot(x_range, x_pred)
-    #
-    # print("Sum of squared errors: ", sum_of_squared_errors(X, y, theta))
-    # print("Average sum of squared errors: ", costs[-1])
-    #
     # plt.show()
 
 
