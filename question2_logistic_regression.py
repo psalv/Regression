32990f1753ecf8521aef9927f0a27d9c50223293
psalv <psalvato@uwo.ca>
2018-04-03 02:08:00 -0400

    Necessary data analysis on test set

--- question2_logistic_regression.py
+++ question2_logistic_regression.py
@@ -30,10 +30,7 @@ def gradient_descent(X, y, theta, alpha, threshold, costs):
     return theta
 
 
-def train_logistic_regression():
-
-    # Getting the data columns
-    data = np.genfromtxt('breast-cancer-wisconsin.test.data.csv', delimiter=',')
+def load_data(data):
 
     # Adding column of 1s for theta0
     X = np.transpose(np.concatenate([np.transpose(np.ones([len(data), 1])), np.transpose(data[:, 1:-1])]))
@@ -46,12 +43,19 @@ def train_logistic_regression():
         else:
             y[i] = 1
 
+    return X, y
+
+
+def train_logistic_regression():
+
+    # Getting the data columns
+    X, y = load_data(np.genfromtxt('breast-cancer-wisconsin.training.data.csv', delimiter=','))
+
     # Initializing thetas
     theta = np.zeros([np.shape(X)[1], 1])
 
     costs = []
     theta = gradient_descent(X, y, theta, 0.1, 10E-8, costs)
-    print(theta)
 
     # # Plotting the learning curve
     # plt.plot(costs)
@@ -59,6 +63,45 @@ def train_logistic_regression():
     # plt.xlabel("Iteration")
     # plt.show()
 
+    return theta
 
-train_logistic_regression()
 
+def predict_logistic_regression(theta, threshold=0.85):
+    X, y = load_data(np.genfromtxt('breast-cancer-wisconsin.test.data.csv', delimiter=','))
+
+    y_pred = np.zeros([np.size(y), 1])
+    sigmoids = sigmoid(theta, X)
+    for i in range(np.size(sigmoids)):
+        if sigmoids[i] > threshold:
+            y_pred[i] = 1
+
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+    for i in range(np.size(y)):
+        if y[i] == 0:
+            if y_pred[i] == 0:
+                true_negative += 1
+            else:
+                false_positive += 1
+        elif y_pred[i] == 0:
+            false_negative += 1
+        else:
+            true_positive += 1
+
+    print("Confusion Matrix (TP/FP/FN/TN):", true_positive, false_positive, false_negative, true_negative)
+
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+
+    f1_score = 2 * (precision * recall / (precision + recall))
+
+    print("F1 Score: ", f1_score)
+
+
+calculated_theta = train_logistic_regression()
+predict_logistic_regression(calculated_theta)
